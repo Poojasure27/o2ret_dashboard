a58d1fb26e92a59bf1badd4929972fcaaf577b4b
poojs <poojasurendran40@gmail.com>
2024-08-01 22:07:47 +0530

    fixed

--- o2ret_dashboard/utils/supabase/middleware.ts
+++ o2ret_dashboard/utils/supabase/middleware.ts
@@ -1,54 +1,44 @@
-import { createServerSupabaseClient, type CookieOptions } from '@supabase/supabase-js'
-import { NextResponse, type NextRequest } from 'next/server'
+import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { NextResponse, type NextRequest } from 'next/server';
 
 export async function updateSession(request: NextRequest) {
   let response = NextResponse.next({
     request: {
       headers: request.headers,
     },
-  })
+  });
 
   const supabase = createServerSupabaseClient({
-    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     getCookie: (name) => request.cookies.get(name)?.value,
     setCookie: (name, value, options) => {
       request.cookies.set({
         name,
         value,
         ...options,
-      })
-      response = NextResponse.next({
-        request: {
-          headers: request.headers,
-        },
-      })
+      });
       response.cookies.set({
         name,
         value,
         ...options,
-      })
+      });
     },
     removeCookie: (name, options) => {
       request.cookies.set({
         name,
         value: '',
         ...options,
-      })
-      response = NextResponse.next({
-        request: {
-          headers: request.headers,
-        },
-      })
+      });
       response.cookies.set({
         name,
         value: '',
         ...options,
-      })
+      });
     },
-  })
+  });
 
-  await supabase.auth.getUser()
+  await supabase.auth.getUser();
 
-  return response
+  return response;
 }
