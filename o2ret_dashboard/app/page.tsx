dbd8d77bb2b7812591f4cac35d5fdaa15bb284bb
poojs <poojasurendran40@gmail.com>
2024-08-01 22:15:41 +0530

    interface added

--- o2ret_dashboard/app/page.tsx
+++ o2ret_dashboard/app/page.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    
-    <h1>Dashboard</h1>
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4 bg-blue">Dashboard</h1>
+        <Link href="/login" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+          Login
+        </Link>
+      </div>
+    </div>
   );
 }
